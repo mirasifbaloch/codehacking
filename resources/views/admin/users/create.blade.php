ed263de9e91e1b9f3cd8a4dc522e364a00da0d78
mirasifbaloch <asifbaloch@gmail.com>
2018-03-18 14:47:40 +0500

    users page without photos

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,7 +1,51 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Maestro
- * Date: 17/03/2018
- * Time: 3:50 PM
- */
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Create Users</h1>
+
+    {!! Form::open(['method'=>'POST', 'action'=> 'AdminUsersController@store', 'files'=>true]) !!}
+
+        <div class="form-group">
+            {!! Form::label('name', 'Name:') !!}
+            {!! Form::text('name', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('email', 'Email:') !!}
+            {!! Form::text('email', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('role_id', 'Role:') !!}
+            {!! Form::select('role_id', [''=> 'Choose Options'] + $roles , null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('status', 'Status:') !!}
+            {!! Form::select('status', array(1 => 'Active', 0 => 'Not Active'), 0, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('file', 'Image:') !!}
+            {!! Form::file('file', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('password', 'Password:') !!}
+            {!! Form::password('password', ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Create User',['class'=>'btn btn-primary']) !!}
+        </div>
+
+        {!! Form::close() !!}
+
+    @include('includes.form_error')
+
+
+@stop
+
+@section('footer')
+
+@stop
